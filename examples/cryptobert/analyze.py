0fceeb1108516d83a621d3f597aa36a85a25bb65
Daniel Kerrigan <d.kerrigan111@gmail.com>
2025-06-03 10:48:24 -0400

    add colab notebook for example

--- examples/cryptobert/analyze.py
+++ examples/cryptobert/analyze.py
@@ -8,7 +8,16 @@ from saefarer.sae import SAE
 from saefarer.utils import get_default_device
 
 
-def main(dataset_path, sae_path, db_path):
+def main(
+    dataset_path,
+    sae_path,
+    db_path,
+    device,
+    num_features,
+    model_batch_size_sequences,
+    feature_batch_size,
+    total_analysis_tokens,
+):
     """Analyze the SAE"""
 
     print("Loading dataset")
@@ -23,16 +32,19 @@ def main(dataset_path, sae_path, db_path):
 
     print("Creating config")
 
+    device = get_default_device() if device == "auto" else device
+    feature_indices = [] if num_features == -1 else list(range(num_features))
+
     cfg = AnalysisConfig(
-        device=get_default_device(),
+        device=device,
         tokens_column="input_ids",
         attn_mask_column="attention_mask",
         labels=[label for _, label in sorted(model.config.id2label.items())],
-        model_batch_size_sequences=32,
+        model_batch_size_sequences=model_batch_size_sequences,
         model_sequence_length=128,
-        feature_batch_size=128,
-        total_analysis_tokens=10_000_000,
-        feature_indices=[],
+        feature_batch_size=feature_batch_size,
+        total_analysis_tokens=total_analysis_tokens,
+        feature_indices=feature_indices,
         n_example_sequences=10,
         n_context_tokens=5,
     )
@@ -57,20 +69,66 @@ def main(dataset_path, sae_path, db_path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+
+    # inputs
+
     parser.add_argument(
-        "-d",
         "--dataset",
         type=str,
         help="Dataset path",
         default="stocktwits-crypto_tokenized/train",
     )
-    parser.add_argument("-s", "--sae", type=str, help="SAE path", default="sae.pt")
+
+    parser.add_argument("--sae", type=str, help="SAE path", default="sae.pt")
+
     parser.add_argument(
-        "-o",
         "--output",
         type=str,
         default="analysis.db",
         help="Output database file path",
     )
+
+    # config
+
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="auto",
+    )
+
+    parser.add_argument(
+        "--num_features",
+        type=int,
+        default=-1,
+    )
+
+    parser.add_argument(
+        "--model_batch_size_sequences",
+        type=int,
+        default=32,
+    )
+
+    parser.add_argument(
+        "--feature_batch_size",
+        type=int,
+        default=128,
+    )
+
+    parser.add_argument(
+        "--total_analysis_tokens",
+        type=int,
+        default=10_000_000,
+    )
+
     args = parser.parse_args()
-    main(args.dataset, args.sae, args.output)
+
+    main(
+        args.dataset,
+        args.sae,
+        args.output,
+        args.device,
+        args.num_features,
+        args.model_batch_size_sequences,
+        args.feature_batch_size,
+        args.total_analysis_tokens,
+    )
